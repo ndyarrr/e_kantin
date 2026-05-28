557f8622b1e08a0fc5a32f693059a3405590e435
ndyarrr <kopi06236@gmail.com>
2026-05-28 14:52:35 +0700

    upgrade pembeli

--- views/pembeli/sections/beranda.php
+++ views/pembeli/sections/beranda.php
@@ -42,11 +42,41 @@
                                 <img src="<?= $img; ?>" alt="<?= htmlspecialchars($menu['nama_menu']); ?>"
                                     onerror="this.style.display='none'; this.nextElementSibling.style.display='flex';">
                                 <div class="menu-img-placeholder <?= $kat ?>" style="display:none;">
-                                    <!-- svg sama seperti sebelumnya -->
+                                    <?php if ($kat === 'minuman'): ?>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                            <path
+                                                d="M3 2l2.01 18.23C5.13 21.23 5.97 22 7 22h10c1.03 0 1.87-.77 1.99-1.77L21 2H3zm9 17c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm1-9H8V8h5v2z" />
+                                        </svg>
+                                    <?php elseif ($kat === 'snack'): ?>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                            <path
+                                                d="M18.06 22.99h1.66c.84 0 1.53-.64 1.63-1.46L23 5.05h-5V1h-1.97v4.05h-4.97l.3 2.34c1.71.47 3.31 1.32 4.27 2.26 1.44 1.42 2.43 2.89 2.43 5.29v8.05zM1 21.99V21h15.03v.99c0 .55-.45 1-1.01 1H2.01c-.56 0-1.01-.45-1.01-1zm15.03-7c0-4.5-6.72-5-8.99-5-2.28 0-9.03.5-9.03 5h18.02z" />
+                                        </svg>
+                                    <?php else: ?>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                            <path
+                                                d="M11 9H9V2H7v7H5V2H3v7c0 2.12 1.66 3.84 3.75 3.97V22h2.5v-9.03C11.34 12.84 13 11.12 13 9V2h-2v7zm5-3v8h2.5v8H21V2c-2.76 0-5 2.24-5 4z" />
+                                        </svg>
+                                    <?php endif; ?>
                                 </div>
                             <?php else: ?>
                                 <div class="menu-img-placeholder <?= $kat ?>">
-                                    <!-- svg sama seperti sebelumnya -->
+                                    <?php if ($kat === 'minuman'): ?>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                            <path
+                                                d="M3 2l2.01 18.23C5.13 21.23 5.97 22 7 22h10c1.03 0 1.87-.77 1.99-1.77L21 2H3zm9 17c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm1-9H8V8h5v2z" />
+                                        </svg>
+                                    <?php elseif ($kat === 'snack'): ?>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                            <path
+                                                d="M18.06 22.99h1.66c.84 0 1.53-.64 1.63-1.46L23 5.05h-5V1h-1.97v4.05h-4.97l.3 2.34c1.71.47 3.31 1.32 4.27 2.26 1.44 1.42 2.43 2.89 2.43 5.29v8.05zM1 21.99V21h15.03v.99c0 .55-.45 1-1.01 1H2.01c-.56 0-1.01-.45-1.01-1zm15.03-7c0-4.5-6.72-5-8.99-5-2.28 0-9.03.5-9.03 5h18.02z" />
+                                        </svg>
+                                    <?php else: ?>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                            <path
+                                                d="M11 9H9V2H7v7H5V2H3v7c0 2.12 1.66 3.84 3.75 3.97V22h2.5v-9.03C11.34 12.84 13 11.12 13 9V2h-2v7zm5-3v8h2.5v8H21V2c-2.76 0-5 2.24-5 4z" />
+                                        </svg>
+                                    <?php endif; ?>
                                 </div>
                             <?php endif; ?>
                         </div>
@@ -56,13 +86,13 @@
                             <span class="price-tag">Rp. <?= number_format($menu['harga'], 0, ',', '.'); ?></span>
                             <button class="btn-tambah-keranjang"
                                 style="width:100%;margin-top:8px;font-size:12px;padding:7px 10px" onclick="addToCart(
-                                    <?= (int) $menu['id_menu'] ?>,
-                                    '<?= addslashes(htmlspecialchars($menu['nama_menu'])) ?>',
-                                    <?= (int) $menu['harga'] ?>,
-                                    '<?= addslashes($menu['foto_menu'] ?? '') ?>',
-                                    '<?= addslashes(htmlspecialchars($menu['nama_toko'])) ?>',
-                                    <?= (int) $menu['id_toko'] ?>
-                                )">
+                <?= (int) $menu['id_menu'] ?>,
+                '<?= addslashes(htmlspecialchars($menu['nama_menu'])) ?>',
+                <?= (int) $menu['harga'] ?>,
+                '<?= addslashes($menu['foto_menu'] ?? '') ?>',
+                '<?= addslashes(htmlspecialchars($menu['nama_toko'])) ?>',
+                <?= (int) $menu['id_toko'] ?>
+            )">
                                 <i class="fa-solid fa-cart-plus"></i> Tambah
                             </button>
                         </div>
